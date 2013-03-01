d5830f313ce91325f3c028bb9fb579eefd5649c5
Patrik Burström <peter@a4647.research.ltu.se>
2013-03-01 11:59:06 +0100

    nytt rum

--- basic_example/basicServer.js
+++ basic_example/basicServer.js
@@ -56,13 +56,14 @@ N.API.getRooms(function (roomlist) {
             myRoom = roomID._id;
             console.log('Created room ', myRoom);
         });
+
+    } else {
+        myRoom = rooms[0]._id;
+        console.log('Using room ', myRoom);
         N.API.createRoom('myRoom2', function (roomID) {
             myRoom2 = roomID._id;
             console.log('Created room ', myRoom2);
         });
-    } else {
-        myRoom = rooms[0]._id;
-        console.log('Using room ', myRoom);
     }
 });
 
