65b96e5c28ec05df82ff487bbdcfd270f898da21
Patrik Burström <peter@a4647.research.ltu.se>
2013-03-11 12:58:14 +0100

    fix

--- basic_example/basicServer.js
+++ basic_example/basicServer.js
@@ -9,7 +9,7 @@ var express = require('express'),
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-var db.mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
   // yay!
