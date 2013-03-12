6b65f859c287bb39b44536fd151cd7faf157bb10
Patrik Burström <peter@a4647.research.ltu.se>
2013-03-12 11:07:14 +0100

    dynamic cafe buttons

---
+++ basic_example/public/index.js
@@ -0,0 +1,28 @@
+var serverUrl = "/";
+
+window.onload = function () {
+ var getCafeNames = function(callback) {
+
+        var req = new XMLHttpRequest();
+        var url = serverUrl + 'api/getcafenames/';
+
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                callback(req.responseText);
+            }
+        };
+
+        req.open('POST', url, true);
+
+        req.setRequestHeader('Content-Type', 'application/json');
+        //console.log("Sending to " + url + " - " + JSON.stringify(body));
+        req.send(JSON.stringify(body));
+    };
+
+    
+
+    getCafeNames(function (response) {
+        var cafes = response;
+        console.log(cafes);
+    }
+};
