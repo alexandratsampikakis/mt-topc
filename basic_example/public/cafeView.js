abe598e66d957aba622f2ccafa143811e4f8d250
Patrik Burström <peter@a4647.research.ltu.se>
2013-03-13 16:01:04 +0100

    fix7

--- basic_example/public/cafeView.js
+++ basic_example/public/cafeView.js
@@ -1,5 +1,5 @@
 var serverUrl = "/";
-var localStream, room;
+var localStream, room, roomId;
 
 //Retrieves querystrings. ex getQueryString('hello') on page 
 //http://www.mypage.se?hello=world returns 'world'
