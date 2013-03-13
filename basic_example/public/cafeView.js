8e8037da9a89f91180babbb7136968a52794f5dd
Patrik Burström <peter@a4647.research.ltu.se>
2013-03-13 19:37:16 +0100

    tset

--- basic_example/public/cafeView.js
+++ basic_example/public/cafeView.js
@@ -29,6 +29,16 @@ window.onload = function () {
     var table5 = document.getElementById('table5');
     var table6 = document.getElementById('table6');
 
+    var getQueryString = function getQueryString(key, default_) {
+        if (default_==null) default_=""; 
+        key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+        var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+        var qs = regex.exec(window.location.href);
+        if(qs == null)
+            return default_;
+        else
+            return qs[1];
+    }
     
     var getCafeTables = function(cafe, callback) {
 
