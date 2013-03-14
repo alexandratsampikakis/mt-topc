69e0e6ae05bd8b7a4ba1af8399a494a4d1e4e510
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-03-14 14:15:26 +0100

    clean up

--- basic_example/public/cafeView.js
+++ basic_example/public/cafeView.js
@@ -1,75 +1,24 @@
-var serverUrl = "/";
-var localStream, room;
-//var tableId1, tableId2, tableId3, tableId4, tableId5, tableId6;
-//Retrieves querystrings. ex getQueryString('hello') on page 
-//http://www.mypage.se?hello=world returns 'world'
-tableId1 = '513dd08a07aa2f143700001f';
-tableId2 = '513dd08b07aa2f1437000020';
-tableId3 = '513dd08c07aa2f1437000021';
-tableId4 = '513dd08d07aa2f1437000022';
-tableId5 = '513dd08e07aa2f1437000023';
-tableId6 = '513dd08f07aa2f1437000024';
+var room, roomId, localStream, serverUrl;
+
+roomId1 = "513dd08a07aa2f143700001f";
+roomId2 = "513dd08b07aa2f1437000020";
+roomId3 = "513dd08c07aa2f1437000021";
+roomId4 = "513dd08d07aa2f1437000022";
+
+serverUrl = "http://satin.research.ltu.se:3001/";
 
 window.onload = function () {
+try {
+  localStream = Erizo.Stream({audio: true, video: true, data: true});
+} catch (error) {
+    console.log('erizo error: ' + error);
+}
 
-    try {
-        localStream = Erizo.Stream({audio: true, video: true, data: true});
-    } catch (error) {
+    var room1 = document.getElementById('room1');
+    var room2 = document.getElementById('room2');
+    var room3 = document.getElementById('room3');
+    var room4 = document.getElementById('room4');
 
-    }   
-
-    var table1 = document.getElementById('table1');
-    var table2 = document.getElementById('table2');
-    var table3 = document.getElementById('table3');
-    var table4 = document.getElementById('table4');
-    var table5 = document.getElementById('table5');
-    var table6 = document.getElementById('table6');
-
-  /*  var getQueryString = function getQueryString(key, default_) {
-        if (default_==null) default_=""; 
-        key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-        var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
-        var qs = regex.exec(window.location.href);
-        if(qs == null)
-            return default_;
-        else
-            return qs[1];
-    }
-    
-    var getCafeTables = function(cafe, callback) {
-
-        var req = new XMLHttpRequest();
-        var url = serverUrl + 'api/getcafe/' + cafe;
-
-        req.onreadystatechange = function () {
-            if (req.readyState === 4) {
-                callback(req.responseText);
-            }
-        };
-
-        req.open('GET', url, true);
-
-        //console.log("Sending to " + url + " - " + JSON.stringify(body));
-        req.send();
-    };
-
-    getCafeTables(getQueryString('cafe'), function (response) {
-        var cafes = JSON.parse(response);
-        var tc = document.getElementById("tableContainer");
-        if(cafes.hasOwnProperty('error')) {
-            console.log(cafes.error);
-        } else {
-            tableId1 = cafes.table1;
-            tableId2 = cafes.table2;
-            tableId3 = cafes.table3;
-            tableId4 = cafes.table4;
-            tableId5 = cafes.table5;
-            tableId6 = cafes.table6;
-            console.log(tableId1);
-        }
-        console.log(cafes.name);
-
-    });*/
 
     var createToken = function(roomId, userName, role, callback) {
 
@@ -90,36 +39,32 @@ window.onload = function () {
         req.send(JSON.stringify(body));
     };
 
-    table1.onclick = function(evt) {
-        initialize(tableId1);
+    room1.onclick = function(evt) {
+        initialize(roomId1);
     };
 
-    table2.onclick = function(evt) {
-        initialize(tableId2);
-    };
-    table3.onclick = function(evt) {
-        initialize(tableId3);
-    };
-    table4.onclick = function(evt) {
-        initialize(tableId4);
-    };
-    table5.onclick = function(evt) {
-        initialize(tableId5);
-    };
-    table6.onclick = function(evt) {
-        initialize(tableId6);
+    room2.onclick = function(evt) {
+        initialize(roomId2);
     };
 
-      var initialize = function(roomId) {
-        var tableContainer = document.getElementById("tableContainer");
-        tableContainer.setAttribute("class", "span8 mute hide");
-        var videoContainer = document.getElementById("videoContainer");
-        videoContainer.setAttribute("class", "span8 mute");
+    room3.onclick = function(evt) {
+        initialize(roomId3);
+    };
+
+    room4.onclick = function(evt) {
+        window.location = './cube/cube.html';
+    };
+
+    var initialize = function(roomId) {
+        var roomcontainer = document.getElementById("roomcontainer");
+        roomcontainer.setAttribute("class", "hide");
+        var vidcontainer = document.getElementById("vidcontainer");
+        vidcontainer.setAttribute("class", "");
 
         createToken(roomId, "user", "role", function (response) {
             var token = response;
             console.log('token created ', token);
-            L.Logger.setLogLevel(L.Logger.DEBUG);
+        L.Logger.setLogLevel(L.Logger.DEBUG);
             //L.Logger.debug("Connected!");
             room = Erizo.Room({token: token});
 
@@ -153,10 +98,10 @@ window.onload = function () {
                 room.addEventListener("stream-subscribed", function(streamEvent) {
                     var stream = streamEvent.stream;
                     var div = document.createElement('div');
-                    div.setAttribute("style", "width: 320px; height: 240px;");
+                    div.setAttribute("style", "width: 320px; height: 240px; position: absolute; top: 10px; left: 10px; background-color: black");
                     div.setAttribute("id", "test" + stream.getID());
 
-                    for (var i = 2; i <= 6; i++) {
+                    for (var i = 2; i < 5; i++) {
                         var elem = document.getElementById('vid'+i);
                         if (elem.childNodes.length === 1) {
                             elem.appendChild(div);
