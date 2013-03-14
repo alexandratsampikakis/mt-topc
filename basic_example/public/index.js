58cdd20329fa93bd3aec198f0585550e1786a7c3
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-03-14 15:36:14 +0100

    test

--- basic_example/public/index.js
+++ basic_example/public/index.js
@@ -24,10 +24,10 @@ window.onload = function () {
         for (var i = 0; i < cafes.cafe.length; i++) {
             var a = document.createElement('a');
             var cafeName = cafes.cafe[i].name;
-            //a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html?cafe="+cafeName);
-            a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html/");
+            a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html?cafe="+cafeName);
             a.innerHTML += cafeName;
             cb.appendChild(a);
+            a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html");
         };
         console.log(cafes.cafe[0].name);
 
