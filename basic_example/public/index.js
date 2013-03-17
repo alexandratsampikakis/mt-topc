47097bcc5154e8fe2e4c9714f0647ea3fa927e91
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-03-17 22:35:29 +0100

    kjhkgh

--- basic_example/public/index.js
+++ basic_example/public/index.js
@@ -24,7 +24,6 @@ window.onload = function () {
         for (var i = 0; i < cafes.cafe.length; i++) {
             var a = document.createElement('a');
             var cafeName = cafes.cafe[i].name;
-            //a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html?cafe="+cafeName);
             a.innerHTML += cafeName;
             cb.appendChild(a);
             a.setAttribute("href", "http://satin.research.ltu.se:3001/cafeView.html?cafe=" + cafeName);
