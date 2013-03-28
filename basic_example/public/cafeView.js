e86695b04cad98c8ca4369b3db69f0bd79a7da70
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-03-28 13:46:12 +0100

    gfg

--- basic_example/public/cafeView.js
+++ basic_example/public/cafeView.js
@@ -186,7 +186,7 @@ try {
     };
     var askToJoinTablePopup = function() {
         //Show popup with: username, progress bar (time left to answer and a yes button)
-        $('.top-right').notify({ type: '.bangTidy' message: { text: 'Aw yeah, It works!' }}).show();
+        $('.top-right').notify({ type: '.bangTidy', message: { text: 'Aw yeah, It works!' }}).show();
     };
 
     var initialize = function(roomId) {
