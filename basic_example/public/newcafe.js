7d40e929c1cbc7a261ac4dd88202504cb813136b
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-07 21:43:52 +0200

    dsf

--- basic_example/public/newcafe.js
+++ basic_example/public/newcafe.js
@@ -520,7 +520,7 @@ function getSnapshots() {
         $('#chatArea').width('100%');
         $('#chatMessage').width('80%');
         $('#sendMessage').width('19%');
-        $('#chatArea').css('margin-top', $('#chatArea').parent().height()-$('#chatArea').height()-$('#bar-top').height()-190);
+        $('#chatArea').css('margin-top', $('#chatArea').parent().height()-$('#chatArea').height()-$('#bar-top').height());
         $('#chatMessage').focus();
 
         localStream.addEventListener("access-accepted", function () {
