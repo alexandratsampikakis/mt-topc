4456381d40760e7b343563a8fa8b15e26ef01e30
Patrik <patrik@patrik-Aspire-3820T.(none)>
2013-04-11 12:33:09 +0200

    rewrwe

--- basic_example/basicServer.js
+++ basic_example/basicServer.js
@@ -116,7 +116,7 @@ tableImgSchema.plugin(ttl, { ttl: 0 });
 app.get("/api/getTableImg/:room", function (req, res) {
     var roomID = req.params.room;
 
-    console.log(roomID);
+    console.log(req.params.room);
     var tableImgModel = mongoose.model('tableImgModel', tableImgSchema);
 
     tableImgModel.findOne({roomID: roomID }, function (err, records) {
