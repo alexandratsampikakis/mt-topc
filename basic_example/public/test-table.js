d5ae58bd5c8d6d015ea4cc9a29948bc97f1d4dc4
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-15 23:37:27 +0200

    test

---
+++ basic_example/public/test-table.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+    $('#myTab a[href=#address]').click(function () {
+        $(this).tab('show');
+    });
+    $('#myTab a[href=#contact]').click(function () {
+        $(this).tab('show');
+    });
+    $('#myTab a[href=#settings]').click(function () {
+        $(this).tab('show');
+    });
+
+};
