5e4eca21887c1e8ecef4a7c21ddef3ca8207b129
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-16 17:50:32 +0200

    test

--- basic_example/public/tab.js
+++ basic_example/public/tab.js
@@ -1,13 +1,9 @@
-window.onload = function () {
-
-    $('#myTab a[href=#napkin]').click(function () {
-        $(this).tab('show');
-    });
-    $('#myTab a[href=#shareVideo]').click(function () {
-        $(this).tab('show');
-    });
-    $('#myTab a[href=#shareDoc]').click(function () {
-        $(this).tab('show');
-    });
-
-};
+$('#myTab a[href=#napkin]').click(function () {
+    $(this).tab('show');
+});
+$('#myTab a[href=#shareVideo]').click(function () {
+    $(this).tab('show');
+});
+$('#myTab a[href=#shareDoc]').click(function () {
+    $(this).tab('show');
+});
