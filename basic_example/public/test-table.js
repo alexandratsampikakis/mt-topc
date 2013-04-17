b452479bed3d202b93d988f278abc7556b133b98
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-17 13:00:58 +0200

    sdfdsf

---
+++ basic_example/public/test-table.js
@@ -0,0 +1,9 @@
+$('#myTab a[href=#napkin]').click(function () {
+    $(this).tab('show');
+});
+$('#myTab a[href=#shareVideo]').click(function () {
+    $(this).tab('show');
+});
+$('#myTab a[href=#shareDoc]').click(function () {
+    $(this).tab('show');
+});
