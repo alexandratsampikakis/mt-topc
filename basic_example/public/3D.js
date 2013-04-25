85d8a11d8ce3fcce42248980c9c47a9c86ffdf66
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-25 21:35:37 +0200

    s

--- basic_example/public/3D.js
+++ basic_example/public/3D.js
@@ -25,81 +25,69 @@ var render = function () {
 
 render();*/
 
-// set the scene size
-var WIDTH = 400,
-  HEIGHT = 300;
+	// set the scene size
+	var WIDTH = 100%,
+	    HEIGHT = 100%;
 
-// set some camera attributes
-var VIEW_ANGLE = 45,
-  ASPECT = WIDTH / HEIGHT,
-  NEAR = 0.1,
-  FAR = 10000;
+	// set some camera attributes
+	var VIEW_ANGLE = 45,
+	    ASPECT = WIDTH / HEIGHT,
+	    NEAR = 0.1,
+	    FAR = 10000;
 
-// set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
+	// get the DOM element to attach to
+	// - assume we've got jQuery to hand
+	var $container = $('#container');
 
-// set up the sphere vars
-var radius = 50,
-    segments = 16,
-    rings = 16;
+	// create a WebGL renderer, camera
+	// and a scene
+	var renderer = new THREE.WebGLRenderer();
+	var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
+	                                ASPECT,
+	                                NEAR,
+	                                FAR  );
+	var scene = new THREE.Scene();
 
-// get the DOM element to attach to
-// - assume we've got jQuery to hand
-var $container = $('#container');
+	// the camera starts at 0,0,0 so pull it back
+	camera.position.z = 300;
 
-// create a WebGL renderer, camera
-// and a scene
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera(
-    VIEW_ANGLE,
-    ASPECT,
-    NEAR,
-    FAR);
+	// start the renderer
+	renderer.setSize(WIDTH, HEIGHT);
 
-var scene = new THREE.Scene();
+	// attach the render-supplied DOM element
+	$container.append(renderer.domElement);
 
-// the camera starts at 0,0,0
-// so pull it back
-camera.position.z = 300;
+	// create the sphere's material
+	var sphereMaterial = new THREE.MeshLambertMaterial(
+	{
+	    color: 0xCC0000
+	});
 
-// start the renderer
-renderer.setSize(WIDTH, HEIGHT);
+	// set up the sphere vars
+	var radius = 50, segments = 16, rings = 16;
 
-// attach the render-supplied DOM element
-$container.append(renderer.domElement);
+	// create a new mesh with sphere geometry -
+	// we will cover the sphereMaterial next!
+	var sphere = new THREE.Mesh(
+	   new THREE.SphereGeometry(radius, segments, rings),
+	   sphereMaterial);
 
+	// add the sphere to the scene
+	scene.add(sphere);
 
-// create a new mesh with
-// sphere geometry - we will cover
-// the sphereMaterial next!
-var sphere = new THREE.Mesh( new THREE.SphereGeometry(
-    radius,
-    segments,
-    rings),
-  sphereMaterial);
+	// and the camera
+	scene.add(camera);
 
-// create the sphere's material
-var sphereMaterial = new THREE.MeshLambertMaterial({
-      color: 0xCC0000
-    });
+	// create a point light
+	var pointLight = new THREE.PointLight( 0xFFFFFF );
 
-// create a point light
-var pointLight = new THREE.PointLight(0xFFFFFF);
+	// set its position
+	pointLight.position.x = 10;
+	pointLight.position.y = 50;
+	pointLight.position.z = 130;
 
-scene.add(camera);
-scene.add(sphere);
-scene.add(pointLight);
+	// add to the scene
+	scene.add(pointLight);
 
-// set the geometry to dynamic
-// so that it allow updates
-sphere.geometry.dynamic = true;
-
-// changes to the vertices
-sphere.geometry.__dirtyVertices = true;
-
-// changes to the normals
-sphere.geometry.__dirtyNormals = true;
-
-renderer.render(scene, camera);
+	// draw!
+	renderer.render(scene, camera);
