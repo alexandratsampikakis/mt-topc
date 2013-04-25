01db74fa98fad3ebe645e013ffeaf5a9ecfada46
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-25 21:26:16 +0200

    sdfg

--- basic_example/public/3D.js
+++ basic_example/public/3D.js
@@ -1,4 +1,4 @@
-var scene = new THREE.Scene();
+/*var scene = new THREE.Scene();
 
 // PerspectiveCamera(field of view,aspect ratio,near,far)
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -12,20 +12,68 @@ var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-var sphereGeo = new THREE.SpheroGeometry(0.5, 0.5, 0.5);
-var sMaterial = new THREE.MeshBasicMaterial({color: 0xe261b7});
-var sphere = new THREE.Mesh(sphereGeo, sMaterial);
-scene.add(sphere);
-
 camera.position.z = 5;
 
 var render = function () {
 	requestAnimationFrame(render);
 
-	//cube.rotation.x += 0.1;
-	//cube.rotation.y += 0.1;
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
 
 	renderer.render(scene, camera);
 };
 
-render();
+render();*/
+
+// set the scene size
+var WIDTH = 400,
+  HEIGHT = 300;
+
+// set some camera attributes
+var VIEW_ANGLE = 45,
+  ASPECT = WIDTH / HEIGHT,
+  NEAR = 0.1,
+  FAR = 10000;
+
+// set up the sphere vars
+var radius = 50,
+    segments = 16,
+    rings = 16;
+
+// get the DOM element to attach to
+// - assume we've got jQuery to hand
+var $container = $('#container');
+
+// create a WebGL renderer, camera
+// and a scene
+var renderer = new THREE.WebGLRenderer();
+var camera = new THREE.PerspectiveCamera(
+    VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR);
+
+var scene = new THREE.Scene();
+
+// the camera starts at 0,0,0
+// so pull it back
+camera.position.z = 300;
+
+// start the renderer
+renderer.setSize(WIDTH, HEIGHT);
+
+// attach the render-supplied DOM element
+$container.append(renderer.domElement);
+
+
+// create a new mesh with
+// sphere geometry - we will cover
+// the sphereMaterial next!
+var sphere = new THREE.Mesh( new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings),
+  sphereMaterial);
+
+scene.add(camera);
+scene.add(sphere);
