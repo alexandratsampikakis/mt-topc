d780fee3a447970f9ec29b62111ff61939396cc8
Patrik Burström <patbur-8@student.ltu.se>
2013-04-29 12:39:56 +0200

    gfdgdf
    gdfgfd

--- basic_example/public/3D.js
+++ basic_example/public/3D.js
@@ -1,6 +1,12 @@
 /*var scene = new THREE.Scene();
+=======
+var room, localStream, serverUrl;
+var tableId = "513dcfda07aa2f143700001c";
+serverUrl = "http://satin.research.ltu.se:3001/";
+>>>>>>> gfdgfd
 
-// PerspectiveCamera(field of view,aspect ratio,near,far)
+var vid, videoTexture, material, geometry, streamer, videoImageContext;
+var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
@@ -10,19 +16,163 @@ document.body.appendChild(renderer.domElement);
 var geometry = new THREE.CubeGeometry(1,1,1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
 camera.position.z = 5;
 
-//var render = function () {
-	//requestAnimationFrame(render);
+function initVideo() {
+    vid = document.getElementById('streamundefined');
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+    
+    vid.style.width = '320px';
+    vid.style.height = '240px';
+    vid.autoplay = true;
+    vidImg = document.getElementById('videoImage');
+    videoImageContext = vidImg.getContext('2d');
+    /*videoTexture = new THREE.Texture( vid );
+    material   = new THREE.MeshLambertMaterial({
+      map : videoTexture
+        });
+    geometry    = new THREE.PlaneGeometry( 1, 1 );
+    streamer = new THREE.Mesh(geometry, material);
+    scene.add(streamer);*/
 
-	renderer.render(scene, camera);
-//};
 
+    videoTexture = new THREE.Texture( vidImg );
+    videoTexture.minFilter = THREE.LinearFilter;
+    videoTexture.magFilter = THREE.LinearFilter;
+    
+    var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
+    // the geometry on which the movie will be displayed;
+    //      movie image will be scaled to fit these dimensions.
+    var movieGeometry = new THREE.PlaneGeometry(  2, 2 );
+    var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
+    movieScreen.position.set(0,0,0);
+    scene.add(movieScreen);
+}
+
+function render() 
+{   
+    requestAnimationFrame(render);
+    if ( vid.readyState === vid.HAVE_ENOUGH_DATA ) 
+    {
+        videoImageContext.drawImage( vid, 0, 0, vidImg.width, vidImg.height );
+        if ( videoTexture ) 
+            videoTexture.needsUpdate = true;
+    }
+
+    renderer.render( scene, camera );
+}
+window.onload = function () {
+
+	try {
+      localStream = Erizo.Stream({audio: true, video: true, data: true});
+    } catch (error) {
+        console.log('erizo error: ' + error);
+    }
+    //Creates token for the chosen café
+    var createToken = function(roomId, userName, role, callback) {
+        var req = new XMLHttpRequest();
+        var url = serverUrl + 'createToken/' + roomId;
+        var body = {username: userName, role: role};
+
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
+                callback(req.responseText);
+            }
+        };
+
+        req.open('POST', url, true);
+
+        req.setRequestHeader('Content-Type', 'application/json');
+        //console.log("Sending to " + url + " - " + JSON.stringify(body));
+        req.send(JSON.stringify(body));
+    };
+
+    var initialize = function(roomId) {
+        //
+        createToken(roomId, "user", "role", function (response) {
+            var token = response;
+            console.log('token created ', token);
+            L.Logger.setLogLevel(L.Logger.DEBUG);
+            //L.Logger.debug("Connected!");
+            room = Erizo.Room({token: token});
+
+            localStream.addEventListener("access-accepted", function () {
+                
+                var subscribeToStreams = function (streams) {
+                    if (!localStream.showing) {
+                        localStream.show();
+                    }
+                    var index, stream;
+                    for (index in streams) {
+                        if (streams.hasOwnProperty(index)) {
+                            stream = streams[index];
+                            if (localStream !== undefined && localStream.getID() !== stream.getID()) {
+                                room.subscribe(stream);
+                            } else {
+                                console.log("My own stream");
+                            }
+                        }
+                    }
+                };
+
+                room.addEventListener("room-connected", function (roomEvent) {
+                    // Publish my stream
+                    room.publish(localStream);
+
+                    // Subscribe to other streams
+                    subscribeToStreams(roomEvent.streams);
+                    console.log("streams: " + roomEvent.streams.length);
+                });
+
+                room.addEventListener("stream-subscribed", function(streamEvent) {
+                    var stream = streamEvent.stream;
+                    
+                    for (var i = 2; i <= 6; i++) {
+                        if ($('#vid'+i).children().length === 0) {
+                            $('<div></div>', {
+                                id: 'test'+stream.getID()
+                            }).css('width','100%').appendTo('#vid'+i);
+                            stream.show("test" + stream.getID());
+                            return;
+                        }
+                    }
+
+                    console.log("There is no seat available at this table!");
+                });
+
+                room.addEventListener("stream-added", function (streamEvent) {
+                    // Subscribe to added streams
+                    var streams = [];
+                    streams.push(streamEvent.stream);
+                    subscribeToStreams(streams);
+
+                    //If table is empty, become the leader
+                    var currStreams = room.getStreamsByAttribute('type','media');
+                    initVideo();
+                    render();
+                });
+
+                room.addEventListener("stream-removed", function (streamEvent) {
+                    // Remove stream from DOM
+                    var stream = streamEvent.stream;
+                    if (stream.elementID !== undefined) {
+                        console.log('stream: ' + stream.getID());
+                        console.log(stream.getID() === leader);
+                        console.log('leader: ' + leader);
+                        if(stream.getID() === leader) {
+                            console.log('kommer jag hit?');
+                            leader = getLeader();
+                            console.log(getLeader());
+                        }
+                        console.log("Removing " + stream.elementID);
+                        $('#'+stream.elementID).remove();
+                    }
+                });
+
+                room.connect();        
+
+                localStream.show("myVideo");
 //render();*/
 
 navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.getUserMedia;
@@ -186,3 +336,9 @@ function render()
 
 	renderer.render( scene, camera );
 }
+            });
+            localStream.init();
+        });   
+    }
+    initialize(tableId);
+};
