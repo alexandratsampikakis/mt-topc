f73297fb63f4a395fcf92a820b4f66b5197c546a
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-04-30 15:11:28 +0200

    sdggh

--- basic_example/public/3D.js
+++ basic_example/public/3D.js
@@ -70,6 +70,7 @@ function initVideo(stream) {
     var movieGeometry = new THREE.PlaneGeometry(  1, 1 );
     var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
     movieScreen.position.set(1*count,1*count,0);
+    movieScreen.rotation.x += 0.5;
     scene.add(movieScreen);
     var newStream = new StreamObject(vid, videoTexture, videoImageContext);
     streams.push(newStream);
