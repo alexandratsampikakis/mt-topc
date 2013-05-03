173ecb7121dfc23b8d332feb87d0f8a46c5f41c4
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-05-03 13:41:44 +0200

    gfdgsd

--- basic_example/public/3D.js
+++ basic_example/public/3D.js
@@ -8,7 +8,7 @@ var scene = new THREE.Scene();
 var bgScene = new THREE.Scene();
 var bgCam = new THREE.Camera();
 var camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 1000);
-var position = [[],[-11,4,0,0.2*Math.PI],[11,4,0,0.8*Math.PI],[-11,0,0,0.2*Math.PI],[11,0,0,0.8*Math.PI],[-11,-4,0,0.2*Math.PI],[11,-4,0,0.8*Math.PI]];
+var position = [[],[-11,4,0,0.2*Math.PI],[11,4,0,-0.2*Math.PI],[-11,0,0,0.2*Math.PI],[11,0,0,-0.2*Math.PI],[-11,-4,0,0.2*Math.PI],[11,-4,0,-0.2*Math.PI]];
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight-82);
 document.body.appendChild(renderer.domElement);
