7fb619d9bce35993b439969e56af2e23a5b4cb5b
Patrik Burström <peter@a4647.research.ltu.se>
2013-05-07 14:34:29 +0200

    fdsfsd

--- basic_example/public/table.js
+++ basic_example/public/table.js
@@ -155,6 +155,7 @@ function loadImage(imageData, elementID, pos) {
         var videoTexture = new THREE.Texture( myImage );
         videoTexture.minFilter = THREE.LinearFilter;
         videoTexture.magFilter = THREE.LinearFilter;
+        videoTexture.needsUpdate = true;
         //var x = room.getStreamsByAttribute('type','media').length;
         var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
         // the geometry on which the movie will be displayed;
