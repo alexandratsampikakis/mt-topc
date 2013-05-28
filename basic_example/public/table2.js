125201e44ef665a2c35737d41087d787a7608d8e
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-05-28 02:09:13 +0200

    gdfgf

--- basic_example/public/table2.js
+++ basic_example/public/table2.js
@@ -827,6 +827,22 @@ window.onload = function () {
             }
     });
 
+    var askToJoinTablePopup = function(nameOfUser) {
+        knockSound();
+        $('#knocking').notify({ type: 'bangTidy', onYes:function () {dataStream.sendData({id:'popup-answer',user:nameOfUser, answer: true})}, onNo:function () {dataStream.sendData({id:'popup-answer',user:nameOfUser, answer: false})}, onClose:function () {dataStream.sendData({id:'popup-answer',user:nameOfUser, answer: false})}, message: { html: '<p style="color: grey"><b>Hey</b>, ' + nameOfUser +' wants to sit down, is that OK?</p>' }, fadeOut: { enabled: true, delay: knockTimer}}).show();
+    };
+
+    var deniedNotification = function(whatCase) {
+        switch (whatCase) {
+            case 1:
+                $('#answer').notify({ fadeOut: { enabled: true, delay: 5000 }, type: 'bangTidy', question: false, message: { html: '<p style="color: grey"><b>Hey</b>, seems that the users want some privacy at the moment. Try again later!</p>' }}).show();
+                break;
+            case 2:
+                $('#answer').notify({ fadeOut: { enabled: true, delay: 5000 }, type: 'bangTidy', question: false, message: { html: '<p style="color: grey"><b>Hey</b>, all the seats are taken at the moment. Try again later!</p>' }}).show();
+                break;
+           default:
+        }
+    }
 }
 
         //Sends a base64 string to server
