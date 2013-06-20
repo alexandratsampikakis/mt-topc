19b86c1a0260197418f0570003f8b5d3912b520d
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-06-21 00:03:25 +0200

    dfdf

--- basic_example/public/js/3Dsnapshot.js
+++ basic_example/public/js/3Dsnapshot.js
@@ -19,61 +19,61 @@ var sendTableImg = function(cafe, imgData, roomId, callback) {
 
 //Loops through and takes a snapshot of each stream. Merges into one image, sends to server.
 function getSnapshots() {
-    //Width and height of popover where the image will be displayed.
-    var w = 320;
-    var h = 200
+   //Width and height of popover where the image will be displayed.
+   var w = 320;
+   var h = 200
 
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
+   var canvas = document.createElement('canvas');
+   var context = canvas.getContext('2d');
 
-    var height = 240;
-    var width = 320;
+   var height = 240;
+   var width = 320;
 
-    //what the image will look like with 6 media streams
-    //'''''''''''''//
-    //  1   2   3  //
-    //  4   5   6  //
-    //,,,,,,,,,,,,,//
-    canvas.width = 3*width;
-    canvas.height = 2*height;
-    for(var i = 0; i<streams.length;i++) {
-        var y = 0;
-        //if i>2, go to "second row" of image
-        if (i>2) {
-            var y = height;
-        }
-        
-        var vid = streams[i].getVideo();
-        context.drawImage( vid,(i%3)*width, y);
+   //what the image will look like with 6 media streams
+   //'''''''''''''//
+   //  1   2   3  //
+   //  4   5   6  //
+   //,,,,,,,,,,,,,//
+   canvas.width = 3*width;
+   canvas.height = 2*height;
+   for(var i = 0; i<streams.length;i++) {
+       var y = 0;
+       //if i>2, go to "second row" of image
+       if (i>2) {
+           var y = height;
+       }
+       
+       var vid = streams[i].getVideo();
+       context.drawImage( vid,(i%3)*width, y);
 
-    }
+   }
 
-    for(var i = length; i<6;i++) {
-        var y = 0;
-        //if i>2, go to "second row" of image
-        if (i>2) {
-            var y = height;
-        }
-        context.drawImage(chairImg, (i%3)*width, y, width, height);        
-    }
+   for(var i = streams.length; i<6;i++) {
+       var y = 0;
+       //if i>2, go to "second row" of image
+       if (i>2) {
+           var y = height;
+       }
+       context.drawImage(chairImg, (i%3)*width, y, width, height);        
+   }
 
-    //Draw the image on a new canvas in order to rescale.
-    var canvas2 = document.createElement('canvas');
-    var context2 = canvas2.getContext('2d');
+   //Draw the image on a new canvas in order to rescale.
+   var canvas2 = document.createElement('canvas');
+   var context2 = canvas2.getContext('2d');
 
-    var imgData = canvas.toDataURL();
-    var myImage = new Image();
-    canvas2.width = 320;
-    canvas2.height = 200;
-    myImage.onload = function(){
-        context2.drawImage(myImage, 0, 0,w,h);
-        
-        //Convert to base64 and send to server.
-        sendTableImg(cafe, canvas2.toDataURL(), room.roomID, function (response) {
-            console.log(response);
-        });
-    }; 
-    myImage.src = imgData;
+   var imgData = canvas.toDataURL();
+   var myImage = new Image();
+   canvas2.width = 320;
+   canvas2.height = 200;
+   myImage.onload = function(){
+       context2.drawImage(myImage, 0, 0,w,h);
+       
+       //Convert to base64 and send to server.
+       sendTableImg(cafe, canvas2.toDataURL(), room.roomID, function (response) {
+           console.log(response);
+       });
+   }; 
+   myImage.src = imgData;
 }
 
 //Checks if a new users video stream is ready for snapshot
